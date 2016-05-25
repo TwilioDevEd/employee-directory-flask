794f14e4f099c81b8c35dcc17845cd8c770a030b
Victor Shyba <victor.shyba@gmail.com>
2016-05-25 17:53:00 -0300

    Refactor view
    Separate each scenario into a function

--- employee_directory_flask/views.py
+++ employee_directory_flask/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 from . import app
 from twilio import twiml
 from .models import Employee
+NOT_FOUND_MESSAGE = "We did not find the employee you're looking for"
 
 
 @app.route('/')
@@ -11,24 +12,38 @@ def root():
 
 @app.route('/directory/search', methods=['POST'])
 def search():
-    response = twiml.Response()
-    NOT_FOUND_MESSAGE = "We did not find the employee you're looking for"
     name = request.form['Body']
     employees = list(Employee.query.filter(Employee.full_name.contains(name)))
     if len(employees) == 1:
-        employee = employees[0]
-        employee_data = '\n'.join([employee.full_name,
-                                   employee.phone_number,
-                                   employee.email])
-        with response.message(employee_data) as message:
-            message.media(employee.image_url)
+        return _send_single_result(employees)
     elif len(employees) > 1:
-        message = ["We found multiple people, reply with:"]
-        names = [employee.full_name for employee in employees]
-        for i, name in enumerate(names, 1):
-            message.append("%s for %s" % (i, name))
-        message.append("Or start over")
-        response.message('\n'.join(message))
-    else:
-        response.message(NOT_FOUND_MESSAGE)
+        return _send_multiple_results(employees)
+    return _send_not_found()
+
+
+def _send_not_found():
+    response = twiml.Response()
+    response.message(NOT_FOUND_MESSAGE)
+    return str(response)
+
+
+def _send_single_result(employees):
+    response = twiml.Response()
+    employee = employees[0]
+    employee_data = '\n'.join([employee.full_name,
+                               employee.phone_number,
+                               employee.email])
+    with response.message(employee_data) as message:
+        message.media(employee.image_url)
+    return str(response)
+
+
+def _send_multiple_results(employees):
+    response = twiml.Response()
+    message = ["We found multiple people, reply with:"]
+    names = [employee.full_name for employee in employees]
+    for i, name in enumerate(names, 1):
+        message.append("%s for %s" % (i, name))
+    message.append("Or start over")
+    response.message('\n'.join(message))
     return str(response)
